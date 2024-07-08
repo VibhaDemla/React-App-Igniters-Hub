39ea8706a8e6ec46ce6c67a177b2f1832ccbd0bc
VibhaDemla <140154943+VibhaDemla@users.noreply.github.com>
2024-07-08 14:28:05 +0530

    Update Information.js

--- src/components/Information.js
+++ src/components/Information.js
@@ -19,7 +19,13 @@ const Information = () => {
         <h2>Todos</h2>
         <ul>
           {todos.map(todo => (
-            <li key={todo.id}>{todo.title}</li>
+            <li key={todo.id} className="todo-item">
+              <span className="todo-id">{todo.id}.</span>
+              <span className="todo-title">{todo.title}</span>
+              <span className={`todo-status ${todo.completed ? 'completed' : 'pending'}`}>
+                {todo.completed ? 'Completed' : 'Pending'}
+              </span>
+            </li>
           ))}
         </ul>
       </div>
@@ -27,7 +33,9 @@ const Information = () => {
         <h2>Added Data</h2>
         <ul>
           {dataAdded.map((data, index) => (
-            <li key={index}>{data.name} - {data.department}</li>
+            <li key={index} className="data-item">
+              <span>{data.name} - {data.department}</span>
+            </li>
           ))}
         </ul>
       </div>
